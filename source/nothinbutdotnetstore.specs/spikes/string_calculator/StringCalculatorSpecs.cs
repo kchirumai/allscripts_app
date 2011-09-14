2f258028a574043f842a94d75f56f0a9363d1ac0
Kavitha Chirumamilla <kavitha.chirumamilla@allscripts.com>
2011-09-14 16:42:19 -0500

    Pushing new changes

--- source/nothinbutdotnetstore.specs/spikes/string_calculator/StringCalculatorSpecs.cs
+++ source/nothinbutdotnetstore.specs/spikes/string_calculator/StringCalculatorSpecs.cs
@@ -1,4 +1,6 @@
-﻿using Machine.Specifications;
+﻿using System;
+using System.Collections.Generic;
+using Machine.Specifications;
 using developwithpassion.specifications.rhinomocks;
 
 namespace nothinbutdotnetstore.specs.spikes.string_calculator
@@ -10,13 +12,33 @@ namespace nothinbutdotnetstore.specs.spikes.string_calculator
         {
         }
 
-        public class when_observation_name : concern
+        public class when_adding_string_of_numbers : concern
         {
-            It first_observation = () => 
+            static int result;
+
+            //Act
+            Because b = () =>
+                result =new StringCalculator().add_strings("");
+            
+            It should_return_the_sum = () =>
+                result.ShouldEqual(0);
         }
     }
 
     public class StringCalculator
     {
+        public int add_strings(string numbers)
+        {
+            int result = 0;
+            
+            if (string.IsNullOrEmpty(numbers)) return result;
+
+            foreach (string arg in numbers.Split(','))
+            {
+                result = result + Convert.ToInt32(arg.Trim());
+            }
+
+            return result;
+        }
     }
 }
