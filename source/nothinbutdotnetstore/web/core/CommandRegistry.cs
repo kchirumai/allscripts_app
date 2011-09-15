19878544408aa95a1ea779d653f82d23e0a5fd61
Kavitha Chirumamilla <kavitha.chirumamilla@allscripts.com>
2011-09-15 11:41:34 -0500

    Pushing new changes

--- source/nothinbutdotnetstore/web/core/CommandRegistry.cs
+++ source/nothinbutdotnetstore/web/core/CommandRegistry.cs
@@ -6,15 +6,19 @@ namespace nothinbutdotnetstore.web.core
     public class CommandRegistry : ICanFindCommands
     {
         IEnumerable<IProcessARequest> all_commands;
+        IProcessARequest the_special_case;
 
-        public CommandRegistry(IEnumerable<IProcessARequest> all_commands)
+        public CommandRegistry(IEnumerable<IProcessARequest> all_commands, IProcessARequest the_special_case)
         {
             this.all_commands = all_commands;
+            this.the_special_case = the_special_case;
         }
 
         public IProcessARequest get_the_command_that_can_process(IContainRequestInformation the_request)
         {
-            return all_commands.First(x => x.can_handle(the_request));
+
+            return all_commands.FirstOrDefault(x => x.can_handle(the_request))?? the_special_case;
+
         }
     }
 }
